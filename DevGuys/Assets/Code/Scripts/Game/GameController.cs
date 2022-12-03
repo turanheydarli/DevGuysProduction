de44c5109804aa912dbc129270846b8b2909163e
Turan <turan.dh@code.edu.az>
2022-12-03 12:42:35 -0800

    Change player prefab

--- DevGuys/Assets/Code/Scripts/Game/GameController.cs
+++ DevGuys/Assets/Code/Scripts/Game/GameController.cs
@@ -14,7 +14,7 @@ public class GameController : MonoBehaviourPunCallbacks
 
     private void Awake()
     {
-        GameObject player = PhotonNetwork.Instantiate("Prefabs/Characters/Player",
+        GameObject player = PhotonNetwork.Instantiate("Prefabs/Characters/TurtleGuy",
             playerTransforms[PhotonNetwork.LocalPlayer.ActorNumber].position, Quaternion.identity);
         cinemachineVirtualCamera.m_Follow = player.transform.GetChild(2).transform;
     }
