5b0bc47b5e675452acbff457844c2438e2c37cf8
DESKTOP-2O64A2T\LENOVO <rafetmammadoff>
2022-12-04 03:34:17 -0800

    dw

--- DevGuys/Assets/Code/Scripts/UI/Rooms/PlayerListingsMenu.cs
+++ DevGuys/Assets/Code/Scripts/UI/Rooms/PlayerListingsMenu.cs
@@ -86,8 +86,8 @@ namespace Code.Scripts.UI.Rooms
         {
             if (PhotonNetwork.IsMasterClient)
             {
-                SceneManager.LoadScene(1);
-                //PhotonNetwork.LoadLevel(1);
+               // SceneManager.LoadScene(1);
+                PhotonNetwork.LoadLevel(1);
                 PhotonNetwork.CurrentRoom.IsOpen = false;
                 PhotonNetwork.CurrentRoom.IsVisible = false;
             }
